bf4b991dcca5757cb1d8d443ae4c3ac2ac889490
Abhinav Shrivastav <152586934+AbhinavShrivastav12@users.noreply.github.com>
2025-11-08 21:36:41 +0545

    Day1

---
+++ Day1/Hello.py
@@ -0,0 +1,20 @@
+#Introduction
+print(3+2) #addition
+print(3*2) #multiplication
+print(3/2) #division
+print(3-2) #subtraction
+print(3%2) #modulus
+print(3 ** 3) #exponential means that 3 will be multiply for 3 times i.e. 3*3*3 = 27
+print(3 // 2) #floor division  which is like if you divide 3 divide by 2 it will be 1 because 2*1= 2 and 1 will be the answer 
+
+#Data-types
+print(type(10)) #intger
+print(type('ABCD')) #String
+print(type(2.14)) #float
+print(type(2 + 3j)) #complex
+print(type([1,2,3])) #list
+print(type({'name' : 'ABCD'})) #dictionary
+print(type({9.8,3.14,2.5})) #set
+print(type((9.8,2.4,41.2))) #tuple
+print(type(3==3)) #boolean
+print(type(3>=3)) #boolean
