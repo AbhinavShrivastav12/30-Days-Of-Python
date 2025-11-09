f4de01093b2634c139544f588d62ad8ee96df7ef
Abhinav Shrivastav <152586934+AbhinavShrivastav12@users.noreply.github.com>
2025-11-09 22:35:50 +0545

    learn the day2 variable and built in function

---
+++ Day2/Day2_Solution.py
@@ -0,0 +1,34 @@
+# Exercise all soultion are solved here!!!
+# 1. Declare a first name variable and assign a value to it
+first_name = 'Abhinav'
+# 2. Declare a last name variable and assign a value to it
+last_name = 'Shrivastav'
+# 3. Declare a full name variable and assign a value to it
+full_name = 'Abhinav Shrivastav'
+# 4. Declare a country variable and assign a value to it
+country = 'Nepal'
+# 5. Declare a city variable and assign a value to it
+city = 'Lumbini'
+# 6. Declare an age variable and assign a value to it
+age = 23
+# 7. Declare a year variable and assign a value to it
+year = 2002
+# 8. Declare a variable is_married and assign a value to it
+is_married = 'unmarried'
+# 9. Declare a variable is_true and assign a value to it
+is_true = True
+# 10. Declare a variable is_light_on and assign a value to it
+is_light_on = True
+# 11. Declare multiple variable on one line
+first_name, last_name, is_light_on = 'Abhinav' , 'Shrivastav', True
+# Showing the output
+print(first_name) 
+print(last_name)
+print(country)
+print(city)
+print(age)
+print(year)
+print(is_married)
+print(is_true)
+print(is_light_on)
+print(first_name,last_name,is_light_on)
